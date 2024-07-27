05f709048c5f4a2ac13120d2eb97c9992e65aa3e
Venodez <30352734+EltyDev@users.noreply.github.com>
2024-07-27 14:30:38 +0200

    Init repo

---
+++ fabric/src/main/java/fr/elty/pridetags/fabric/client/PridetagsFabricClient.java
@@ -0,0 +1,10 @@
+package fr.elty.pridetags.fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class PridetagsFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
