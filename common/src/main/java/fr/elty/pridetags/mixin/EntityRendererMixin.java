5f59ac774c89ff4edb6be820e293f8babe967734
Venodez <30352734+EltyDev@users.noreply.github.com>
2024-08-22 02:43:24 +0200

    Fix dupe of rendering because of objective displayed below name

--- common/src/main/java/fr/elty/pridetags/mixin/EntityRendererMixin.java
+++ common/src/main/java/fr/elty/pridetags/mixin/EntityRendererMixin.java
@@ -10,11 +10,17 @@ import net.minecraft.client.gui.Font;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.numbers.StyledFormat;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.scores.DisplaySlot;
+import net.minecraft.world.scores.Objective;
+import net.minecraft.world.scores.ReadOnlyScoreInfo;
+import net.minecraft.world.scores.Scoreboard;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
@@ -29,9 +35,21 @@ public abstract class EntityRendererMixin<T extends Entity> {
     @Unique
     private static final int Y_GAP = 3;
 
+    @Unique
+    public boolean isRenderingScore(Player player, Component component) {
+        Scoreboard scoreboard = player.getScoreboard();
+        Objective objective = scoreboard.getDisplayObjective(DisplaySlot.BELOW_NAME);
+        if (objective == null) return false;
+        ReadOnlyScoreInfo readOnlyScoreInfo = scoreboard.getPlayerScoreInfo(player, objective);
+        Component score = ReadOnlyScoreInfo.safeFormatValue(readOnlyScoreInfo, objective.numberFormatOrDefault(StyledFormat.NO_STYLE)).append(CommonComponents.SPACE).append(objective.getDisplayName());
+        if (component.getString().equals(score.getString())) return true;
+        return false;
+    }
+
     @Inject(method = "renderNameTag", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;popPose()V", shift = At.Shift.BEFORE))
     private void renderNameTag(T entity, Component component, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, float f, CallbackInfo ci, @Local Matrix4f matrix4f, @Local Font font, @Local boolean bl, @Local(ordinal = 1) int j, @Local(ordinal = 2) int k) {
         if (!(entity instanceof Player player)) return;
+        if (isRenderingScore(player, component)) return;
         String username = player.getName().getString();
         Profile profile = null;
         try {
@@ -50,7 +68,7 @@ public abstract class EntityRendererMixin<T extends Entity> {
             if (!bl) break;
             if (flag == null) continue;
             RenderSystem.bindTexture(Minecraft.getInstance().getTextureManager().getTexture(flag).getId());
-            VertexConsumer vertexConsumer = multiBufferSource.getBuffer(RenderType.text(flag));
+            VertexConsumer vertexConsumer = multiBufferSource.getBuffer(RenderType.textSeeThrough(flag));
             vertexConsumer.addVertex(matrix4f, padding - totalWidth/2 , -height - j, 0).setColor(255, 255, 255, 255).setUv(0, 1).setLight(i);
             vertexConsumer.addVertex(matrix4f, padding - totalWidth/2, offsetY - height - j, 0).setColor(255, 255, 255, 255).setUv(0, 0).setLight(i);
             vertexConsumer.addVertex(matrix4f,offsetX + padding - totalWidth/2, offsetY - height - j, 0).setColor(255, 255, 255, 255).setUv(1, 0).setLight(i);
