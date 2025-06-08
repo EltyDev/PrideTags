1721cf8230f5fadf7fe37c2e0f07f11c1bd7a75c
Élise Valentin <venodez@gmail.com>
2025-06-08 07:22:00 +0200

    Fix port

--- common/src/main/java/fr/elty/pridetags/Pridetags.java
+++ common/src/main/java/fr/elty/pridetags/Pridetags.java
@@ -1,9 +1,28 @@
 package fr.elty.pridetags;
 
+import dev.architectury.platform.Platform;
+import io.netty.util.internal.ConcurrentSet;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.resources.ResourceLocation;
+
+import java.nio.file.Path;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 public final class Pridetags {
+
     public static final String MOD_ID = "pridetags";
+    public static Path ConfigPath;
+
+    public static Set<Profile> profiles = ConcurrentHashMap.newKeySet();
 
     public static void init() {
-        // Write common init code here.
+        if (Platform.isFabric())
+            ConfigPath = FabricLoader.getInstance().getGameDir().resolve("resources/pridetags_flags/");
+        else {
+            //TODO To change later
+            ConfigPath = FabricLoader.getInstance().getGameDir().resolve("resources/pridetags_flags/");
+        }
+        PronounsAPI.THREAD.start();
     }
 }
